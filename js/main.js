cab2dfe6f2243f2434546fab7e1c7c0068f9e08d
IvanNik85 <ivannikolic0885@gmail.com>
2019-03-30 15:41:40 +0100

    uneta random baza korisnika i login u izradi

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-import {navMeni} from './navbarMenu.js'
+import { navMeni } from './navbarMenu.js'
 navMeni();
 
-$(document).ready(function () {   
+$(document).ready(function () {
     let imeLogin = 'marija';
-    let prezimeLogin = 'markovic';
+    let passLogin = 'matic';
     //napomena - prilikom unosa imena korisnika koristiti toLowerCase
 
-    $('#submit').click(function (event ) {        
+    $('#submit').click(function (event) {
         console.log($('#ime').val())
         potvrda();
         event.preventDefault();
     })
     $('#ime').keypress(enter);
-    $('#prezime').keypress(enter);    
+    $('#prezime').keypress(enter);
 
     function enter(event) {
         if (event.which == 13) {
@@ -20,15 +20,15 @@ $(document).ready(function () {
             event.preventDefault();
         }
     }
-    function potvrda() {  
-        let $logName = $('#ime').val().toLowerCase();   
-         let $logSurname = $('#prezime').val().toLowerCase();  
-        if ($logName == imeLogin && $logSurname == prezimeLogin) {
+    function potvrda() {
+        let $logName = $('#ime').val().toLowerCase();
+        let $logPass = $('#pass').val().toLowerCase();
+        if ($logName == imeLogin && $logPass == passLogin) {
             console.log('Uspeh')
-            $('ul li:last-child').text('Marija').css({color: 'yellow',padding: '8px'})
-            $('form').css('display','none');
+            $('ul li:last-child').text('Marija').css({ color: 'red' })
+            $('form').css('display', 'none');
             $('body').append(`<h1>Dobrodosla ${$logName}<br/>Uspesno ste se Ulogovali</h1>`)
-         } //else if($logName = ' '){
+        } //else if($logName = ' '){
         //     $('#ime').attr('placeholder','molimo unesite ime');
         //     setTimeout(function() {
         //         $('#ime').removeAttr('placeholder')
@@ -36,24 +36,70 @@ $(document).ready(function () {
         // }        
     }
 
-    $("a.nav-link").click(function(){
-        console.log('nesto')        
+    $("a.nav-link").click(function () {
+        console.log('nesto')
         $('.navbar-nav').find(".active").removeClass("active");
         $(this).parent().addClass("active");
         //event.preventDefault();
     });
     $('#scrollTop').click(topFunction);
-    window.onscroll = function() {scrollFunction()};
+    window.onscroll = function () { scrollFunction() };
     function scrollFunction() {
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 1850) {
-            $('#scrollTop').css('display','block');
+            // $(window).scrollTop()
+            $('#scrollTop').css('display', 'block');
         } else {
-            $('#scrollTop').css('display','none');
+            $('#scrollTop').css('display', 'none');
         }
-      }
+    }
     function topFunction() {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
-      }    
+    }
 });
 
+class Pacijent {
+    constructor(ime, prezime, email, telefon, brojKartona) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.email = email;
+        this.telefon = telefon;
+        this.brojKartona = brojKartona;
+    }
+}
+let allPatients = [];
+let patient = {};
+let ime = ['Marko', 'Milos', 'Nenad', 'Zoran', 'Stefan', 'Milan', 'Aleksa',
+    'Marija', 'Milica', 'Anica', 'Bojana', 'Jelena', 'Sanja', 'Natasa',
+    'Gordana', 'Milan', 'Lazar', 'Miljko', 'Svetlana', 'Kristina'];
+let prezime = ['Jovanovic', 'Aleksic', 'Boskovic', 'Miletic', 'Paunovic', 'Krstic', 'Vasic',
+    'Matic', 'Pavic', 'Ognjenovic', 'Milosevic', 'Popovic', 'Petrovic', 'Jevremovic',
+    'Velickovic', 'Sretenovic', 'Smiljanic', 'Milivojevic', 'Urosevic', 'Stankovic'];
+let email = ['gmail', 'yahoo', 'outlook'];
+let mreza = ['061', '062', '063', '064', '065'];
+
+function generate(value) {
+    return value[Math.floor(Math.random() * value.length)];
+}
+// function genTel() {
+//     return `${generate(telefon)}/${number}`
+// }
+function generatePatients(num) { 
+    for(let i=0;i<num;i++) {
+        let number = Math.floor(Math.random() * 9000000);
+        let tel = `${generate(mreza)}/${number}`;
+        patient.ime = generate(ime);
+        patient.prezime = generate(prezime);
+        patient.email = `${patient.ime}.${patient.prezime}@${generate(email)}.com`;
+        allPatients.push(new Pacijent(patient.ime, patient.prezime, patient.email, tel, number));
+    }
+}
+generatePatients(100);
+if(!localStorage.getItem('listaPacijenata')) {
+    let myJSON = JSON.stringify(allPatients);
+    localStorage.setItem('listaPacijenata', myJSON);
+}
+// let name = JSON.stringify(localStorage.getItem('listaPacijenata'));
+
+
+console.log(allPatients)
--- js/navbarMenu.js
+++ js/navbarMenu.js
@@ -2,11 +2,12 @@ export function navMeni() {
 
     $(document).ready(function () {
 
-        let niz = ['Home', 'Info', 'Galerija', 'Kontakt', 'Login'];
+        let niz = ['Home', 'Info', 'Galerija', 'Zakazivanje', 'Kontakt', 'Login'];
         $('body').prepend(`<nav><a href="#home"class="logo"><img src="./images/logo.jpg"></a><a href="##"><i class="fa fa-bars"></a><ul></ul></nav>`);
         for (let i = 0; i < 5; i++) {
-            $('ul').append(`<li><a href="#${niz[i].toLowerCase()}">${niz[i]}</a></li>`);
-        }
+            $('ul').append(`<li><a href="#${niz[i].toLowerCase()}">${niz[i]}</a></li>`);           
+        }        
+        $('ul:last-child').append(`<li><a href="./login/loginUser.html">Login</a></li>`);
 
         let rotate = 'fa-times rotate';
         $('.fa-bars').click(function () {
@@ -14,9 +15,12 @@ export function navMeni() {
             $(this).toggleClass(rotate);
         });
                 
-        $(window).resize(() => {
+        $(window).resize(() => {            
             $(this).width() < 992 && $('nav ul li').hide();
             $('i').removeClass(rotate);
         });
+        $(window).scroll(() => {
+            $(this).scrollTop() > 20 ? $('nav').addClass('navActive'): $('nav').removeClass('navActive'); 
+        })
     })
 }
