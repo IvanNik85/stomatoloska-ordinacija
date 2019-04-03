dea295bc773c79f80c4453e603a5de79c5cdfa1e
IvanNik85 <ivannikolic0885@gmail.com>
2019-04-03 13:32:30 +0200

    korigovana baza i admin filtriranje u toku

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ import { navMeni } from './navbarMenu.js'
 navMeni();
 
 $(document).ready(function () {
-    let imeLogin = 'marija';
-    let passLogin = 'matic';
+    let imeAdmin = 'ivan';
+    let passAdmin = 'nik';
     //napomena - prilikom unosa imena korisnika koristiti toLowerCase
 
-    $('#submit').click(function (event) {
+    $('#submitAdmin').click(function (event) {
         console.log($('#ime').val())
         potvrda();
         event.preventDefault();
@@ -15,33 +15,28 @@ $(document).ready(function () {
     $('#prezime').keypress(enter);
 
     function enter(event) {
-        if (event.which == 13) {
+        if (event.which == 13) {           
             potvrda();
-            event.preventDefault();
+            // event.preventDefault();
         }
     }
     function potvrda() {
         let $logName = $('#ime').val().toLowerCase();
         let $logPass = $('#pass').val().toLowerCase();
-        if ($logName == imeLogin && $logPass == passLogin) {
+        if ($logName == imeAdmin && $logPass == passAdmin) {
             console.log('Uspeh')
-            $('ul li:last-child').text('Marija').css({ color: 'red' })
-            $('form').css('display', 'none');
-            $('body').append(`<h1>Dobrodosla ${$logName}<br/>Uspesno ste se Ulogovali</h1>`)
-        } //else if($logName = ' '){
-        //     $('#ime').attr('placeholder','molimo unesite ime');
-        //     setTimeout(function() {
-        //         $('#ime').removeAttr('placeholder')
-        //     }, 1500)           
-        // }        
+            $('ul li:last-child').hide().html(`<li><a href="##">Ivan</a></li>`).css({ color: 'red' });
+            $('.toggle').hide();
+            $('.adminContent').show();
+            $('.welcome').append(`<h1>Dobrodosao ${$logName}e<br/>Uspesno ste se Ulogovali</h1>`)
+        } else if($logName == ' '){
+            $('#ime1').attr('placeholder','molimo unesite ime');
+            setTimeout(function() {
+                $('#ime1').removeAttr('placeholder')
+            }, 1500)           
+        }        
     }
 
-    $("a.nav-link").click(function () {
-        console.log('nesto')
-        $('.navbar-nav').find(".active").removeClass("active");
-        $(this).parent().addClass("active");
-        //event.preventDefault();
-    });
     $('#scrollTop').click(topFunction);
     window.onscroll = function () { scrollFunction() };
     function scrollFunction() {
@@ -56,50 +51,94 @@ $(document).ready(function () {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     }
+
+    class Pacijent {
+        constructor(ime, prezime, email, telefon, brojKartona) {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.email = email;
+            this.telefon = telefon;
+            this.brojKartona = brojKartona;
+        }
+    }
+    let allPatients = [];
+    let patient = {};
+    let ime = ['Marko', 'Milos', 'Nenad', 'Zoran', 'Stefan', 'Milan', 'Aleksa',
+        'Marija', 'Milica', 'Anica', 'Bojana', 'Jelena', 'Sanja', 'Natasa',
+        'Gordana', 'Milan', 'Lazar', 'Miljko', 'Svetlana', 'Kristina'];
+    let prezime = ['Jovanovic', 'Aleksic', 'Boskovic', 'Miletic', 'Paunovic', 'Krstic', 'Vasic',
+        'Matic', 'Pavic', 'Ognjenovic', 'Milosevic', 'Popovic', 'Petrovic', 'Jevremovic',
+        'Velickovic', 'Sretenovic', 'Smiljanic', 'Milivojevic', 'Urosevic', 'Stankovic'];
+    let email = ['gmail', 'yahoo', 'outlook'];
+    let mreza = ['061', '062', '063', '064', '065'];
+
+    function randNumber() {
+        return Math.floor(Math.random() * 9000000);
+    }
+    function generate(value) {
+        return value[Math.floor(Math.random() * value.length)];
+    }
+
+    // Dodati datum kao objekt ili niz    
+    function generatePatients(num) {
+        for (let i = 0; i < num; i++) {            
+            let phone = `${generate(mreza)}/${randNumber()}`;
+            patient.ime = generate(ime);
+            patient.prezime = generate(prezime);
+            patient.email = `${patient.ime}.${patient.prezime}@${generate(email)}.com`;
+            allPatients.push(new Pacijent(patient.ime, patient.prezime, patient.email, phone, randNumber()));
+        }
+    }
+    generatePatients(100);    
+    if (!localStorage.getItem('listaPacijenata')) {
+        let myJSON = JSON.stringify(allPatients);
+        localStorage.setItem('listaPacijenata', myJSON);
+    }   
+
+    $('.finish').click(function() {
+        let inputName = $('#ime1').val();
+        let inputSurname = $('#prezime1').val();
+        let inputEmail = $('#email').val();
+        let inputPhone = $('#phone').val();
+        
+        if(localStorage.getItem('listaPacijenata')) {
+            allPatients = JSON.parse(localStorage.getItem('listaPacijenata'));
+            allPatients.push(new Pacijent(inputName, inputSurname, inputEmail, inputPhone, randNumber())); 
+            localStorage.setItem('listaPacijenata', JSON.stringify(allPatients));
+            console.log(allPatients);  
+        }        
+    });  
+    
+    $('#allPatients').click(function(){        
+        let showTable = document.querySelector('.table');
+        let table = document.createElement('table');
+        table.setAttribute('id', 'patientsTable') 
+        showTable.appendChild(table);            
+        for(let i=0; i<allPatients.length; i++) {
+            let tr = document.createElement('tr');
+            table.appendChild(tr);             
+            for(let j=0;j<Object.keys(allPatients[0]).length;j++) {
+                if(i==0) {
+                let th = document.createElement('th');
+                th.innerHTML = Object.keys(allPatients[0])[j];
+                tr.appendChild(th);
+                continue;
+            }
+                let td = document.createElement('td');
+                td.innerHTML = Object.values(allPatients[i])[j];
+                tr.appendChild(td);
+            }
+        }     
+    });
+    
+    let filter = [];    
+    $('#filter').click(function(){ 
+        let imeFilter = $('#imePac').val(); 
+            for(let i = 0; i< allPatients.length; i++) {
+            if(imeFilter == allPatients[i].ime) {
+                console.log(allPatients[i])
+            }
+        }
+    });
+    
 });
-
-class Pacijent {
-    constructor(ime, prezime, email, telefon, brojKartona) {
-        this.ime = ime;
-        this.prezime = prezime;
-        this.email = email;
-        this.telefon = telefon;
-        this.brojKartona = brojKartona;
-    }
-}
-let allPatients = [];
-let patient = {};
-let ime = ['Marko', 'Milos', 'Nenad', 'Zoran', 'Stefan', 'Milan', 'Aleksa',
-    'Marija', 'Milica', 'Anica', 'Bojana', 'Jelena', 'Sanja', 'Natasa',
-    'Gordana', 'Milan', 'Lazar', 'Miljko', 'Svetlana', 'Kristina'];
-let prezime = ['Jovanovic', 'Aleksic', 'Boskovic', 'Miletic', 'Paunovic', 'Krstic', 'Vasic',
-    'Matic', 'Pavic', 'Ognjenovic', 'Milosevic', 'Popovic', 'Petrovic', 'Jevremovic',
-    'Velickovic', 'Sretenovic', 'Smiljanic', 'Milivojevic', 'Urosevic', 'Stankovic'];
-let email = ['gmail', 'yahoo', 'outlook'];
-let mreza = ['061', '062', '063', '064', '065'];
-
-function generate(value) {
-    return value[Math.floor(Math.random() * value.length)];
-}
-// function genTel() {
-//     return `${generate(telefon)}/${number}`
-// }
-function generatePatients(num) { 
-    for(let i=0;i<num;i++) {
-        let number = Math.floor(Math.random() * 9000000);
-        let tel = `${generate(mreza)}/${number}`;
-        patient.ime = generate(ime);
-        patient.prezime = generate(prezime);
-        patient.email = `${patient.ime}.${patient.prezime}@${generate(email)}.com`;
-        allPatients.push(new Pacijent(patient.ime, patient.prezime, patient.email, tel, number));
-    }
-}
-generatePatients(100);
-if(!localStorage.getItem('listaPacijenata')) {
-    let myJSON = JSON.stringify(allPatients);
-    localStorage.setItem('listaPacijenata', myJSON);
-}
-// let name = JSON.stringify(localStorage.getItem('listaPacijenata'));
-
-
-console.log(allPatients)
--- js/navbarMenu.js
+++ js/navbarMenu.js
@@ -1,13 +1,27 @@
 export function navMeni() {
 
     $(document).ready(function () {
-
+        var links = [] 
         let niz = ['Home', 'Info', 'Galerija', 'Zakazivanje', 'Kontakt', 'Login'];
         $('body').prepend(`<nav><a href="#home"class="logo"><img src="./images/logo.jpg"></a><a href="##"><i class="fa fa-bars"></a><ul></ul></nav>`);
         for (let i = 0; i < 5; i++) {
-            $('ul').append(`<li><a href="#${niz[i].toLowerCase()}">${niz[i]}</a></li>`);           
-        }        
-        $('ul:last-child').append(`<li><a href="./login/loginUser.html">Login</a></li>`);
+            let link = `./index.html#${niz[i].toLowerCase()}`
+            $('ul').append(`<li><a href="${link}">${niz[i]}</a></li>`);             
+            links.push(`${link}`); 
+        }  
+        $('ul').append(`<li><a href="./login/loginAdmin.html" id="adm">Admin</a></li>`);     
+        $('ul').append(`<li><a href="./login/loginUser.html" id="log">Login</a></li>`);  
+                      
+            // $('#adm, #log').mouseleave(function(){
+            //     let a = $(this).attr('href');               
+            //     let all = document.querySelectorAll('li a');
+            //     if(window.location.pathname == a.slice(1)) {
+            //         console.log('true')
+            //     for (let i in links) {
+            //         all[i].setAttribute('href', '.' + links[i]); 
+            //     }  
+            // }
+            // })
 
         let rotate = 'fa-times rotate';
         $('.fa-bars').click(function () {
